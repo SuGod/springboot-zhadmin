d7f1415913fa8a95e7b08f2ad3e6647f849ad6d0
SuGOd <158059807@qq.com>
2017-12-12 14:30:00 +0800

    first commit

---
+++ src/main/java/cn/zzdee/zhadmin/ZhadminApplication.java
@@ -0,0 +1,12 @@
+package cn.zzdee.zhadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhadminApplication.class, args);
+	}
+}
